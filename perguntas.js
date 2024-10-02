aa937bebceb050b9cb7c87d083a9a13ae0e9756b
schnoremberger <161774267+schnoremberger@users.noreply.github.com>
2024-10-02 19:44:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'GEOGRAFIA',
+    'Onde fica localizado o Brasil ?',
+    'América do Sul'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Quantos continentes existem?',
+    '6'
+)
+
+criaCartao(
+    'Programação',
+    ' O que é maior: gigabyte ou megabyte?',
+    'gigabyte'
+)
+
+criaCartao(
+    'Histótia',
+    'Jesus nasceu em Belém ou Nazaré?',
+    'Belém'
+)
+
+criaCartao(
+    'História',
+    'Em que ano foi usado um celular pela primeira vez no Brasil?',
+    '1990'
+)
+
+criaCartao(
+    'Título 6',
+    'Pergunta 6',
+    'Resposta 6'
+)
+
+criaCartao(
+    'Título 7',
+    'Pergunta 7',
+    'Resposta 7'
+)
+
+criaCartao(
+    'Título 8',
+    'Pergunta 8',
+    'Resposta 8'
+)
+
+criaCartao(
+    'Título 9',
+    'Pergunta 9',
+    'Resposta 9'
+)
+
+criaCartao(
+    'Título 10',
+    'Pergunta 10',
+    'Resposta 10'
+)
